fa4aa42a4b643248ebf21e376d77acb0f2d6e2f4
Techatach <techatach@gmail.com>
2024-03-31 20:11:34 +0700

    first commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Navbar from "../navbar/page";
+import Footer from "../footer/page";
+
+type Props = {};
+
+export default function Contact({}: Props) {
+  return (
+    <div>
+      {<Navbar />}
+      <div className="grid md:block shadow-lg p-3 mb-5 bg-body rounded">
+        <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-72 flex justify-center w-full">
+          <div className="mt-10 text-white text-4xl font-bold">
+            GET IN TOUCH
+          </div>
+        </div>
+        <div className="bg-white h-auto flex justify-center">
+          <div className="bg-white shadow-lg -mt-40 md:w-1/2 grid lg:flex justify-center">
+            <div className="w-3/4 lg:w-2/3 ">
+              <div className="text-lg font-medium text-blue-600 m-6 ">
+                Drop a Message
+              </div>
+              <div className=" flex lg:flex-row flex-col">
+                <div className="m-6">
+                  <p className="text-sm text-stone-400">Full Name</p>{" "}
+                  <input className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                  <p className="text-sm text-stone-400 mt-6">E-mail</p>{" "}
+                  <input className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                </div>
+                <div className="m-6 ">
+                  <p className="text-sm text-stone-400">Phone</p>{" "}
+                  <input className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                  <p className="text-sm text-stone-400 mt-6">Subject</p>{" "}
+                  <input className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                </div>
+              </div>
+              <div className="m-6 ">
+                <p className="text-sm text-stone-400 mt-6 ">Message</p>{" "}
+                <input className="border-b-2 border-stone-400 text-stone-400 w-36" />
+                <div className="m-4 mt-6 pl-4 pt-1 pb-1 pr-4 bg-gradient-to-r from-cyan-500 to-blue-500 rounded-3xl text-white font-medium w-36 ">
+                  Send Message
+                </div>
+              </div>
+            </div>
+            <div className="lg:w-1/3 bg-green-500 ">
+              <div className="text-white m-6 font-medium">
+                {" "}
+                Contact Information{" "}
+              </div>
+              <div className="text-white m-6 text-sm flex">
+                <ion-icon name="location-sharp" className="m-2"></ion-icon>
+                <div> 4329 Travis Street, Red Fort L.A., 34950 </div>
+              </div>
+              <div className="text-white m-6 text-sm flex">
+                <ion-icon name="call-outline" className="m-2"></ion-icon>
+                <div> +1 987-6543-210 </div>
+              </div>
+              <div className="text-white m-6 text-sm flex">
+                <ion-icon name="mail-outline" className="m-2"></ion-icon>
+                <div> admin@tailwind.org </div>
+              </div>
+              <div className="text-white m-6 text-sm flex">
+                <ion-icon name="globe-outline" className="m-2"></ion-icon>
+                <div> www.tailwind.org </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        noModule
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+      ></script>
+      {<Footer />}
+    </div>
+  );
+}
---
+++ src/app/team/page.tsx
@@ -0,0 +1,153 @@
+import React from "react";
+import Navbar from ".././navbar/page";
+import Footer from "../footer/page";
+
+type Props = {};
+
+export default function Team({}: Props) {
+  return (
+    <div className="">
+      {<Navbar />}
+      <div className="flex items-center justify-center text-center shadow-lg mb-5 bg-body rounded">
+        <div className="flex flex-col p-2 m-2">
+          <div className="text-3xl font-medium">
+            อาจารย์ผู้รับผิดชอบหลักสูตร
+          </div>
+          <div className="text-sm mx-2 py-5 md:text-xl text-stone-500">
+            หลักสูตรรัฐประศาสนศาสตร์ มีอาจารย์ผู้รับผิดชอบหลักสูตรทั้งหมด 6 ท่าน
+            ดังนี้
+          </div>
+          {/* <div className="text-sm mx-2 md:text-xl mb-2 text-stone-500">
+            Resource person of Tailblocks communinty
+          </div> */}
+          <div className="md:grid flex">
+            <div className="flex items-center justify-center md:flex-row flex-col space-x-4 p-2">
+              <div className="flex items-center justify-center flex-col px-6 py-2  max-w-7xl">
+                {/* <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200"> */}
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    // src="https://source.unsplash.com/300x300/?man,businessman"
+                    src="images/te.png"
+                    alt=""
+                    className=""
+                  />
+                </div>
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-green-500 pt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+                <div className=" italic text-gray-500">
+                  การเปลี่ยนแปลงและการพัฒนาองค์การภาครัฐ
+                </div>
+                <div className=" italic text-gray-500">
+                  การวางแผนและการบริหารนโยบายสาธาณะในยุคดิจิทัล
+                </div>
+              </div>
+              <div className="flex items-center justify-center flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?boy"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400 pt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+              </div>
+              <div className="flex items-center justify-center flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?girl"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400 pt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+              </div>
+            </div>
+            <div className="flex space-x-4  p-2 items-center justify-center md:flex-row flex-col ">
+              <div className="flex items-center justify-center flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?kid"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400 pt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+              </div>
+              <div className="flex items-center justify-center flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?girl,woman"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400 pt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+              </div>
+              <div className="flex items-center justify-center flex-col px-6 py-2 ">
+                <div className="md:h-40 h-28 w-28 md:w-40 rounded-full overflow-hidden bg-gray-200">
+                  <img
+                    src="https://source.unsplash.com/300x300/?woman"
+                    alt=""
+                    className=""
+                  />
+                </div>
+
+                <div className="text-lg font-medium text-stone-600 cursor-pointer hover:text-stone-400 pt-5">
+                  อ.ดร.เตชทัช คลายโศกก์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+                <div className=" italic text-gray-500">
+                  การวิเคราะห์ข้อมูลขนาดใหญ่ สำหรับรัฐประศาสนศาสตร์
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script src="https://use.fontawesome.com/03f8a0ebd4.js"></script>
+      <script
+        type="module"
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"
+      ></script>
+      <script
+        noModule
+        src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"
+      ></script>
+      {<Footer />}
+    </div>
+  );
+}
